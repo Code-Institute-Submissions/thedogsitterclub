85bff23eae718f2c943268aa9c34e56dd9eac35d
Axel Zwaans <axelzwaans@hotmail.com>
2023-03-12 17:45:24 +0000

    Add react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <h1>The Petsitter Club</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
