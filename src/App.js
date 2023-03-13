14f0d1af462c61e54bc33d3c79cd79a36867976a
Axel Zwaans <axelzwaans@hotmail.com>
2023-03-13 06:28:21 +0000

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Button from 'react-bootstrap/Button';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className='App'>
+      <NavBar />
       <h1>The Petsitter Club</h1>
-      <Button variant="primary">Primary</Button>
     </div>
   );
 }
